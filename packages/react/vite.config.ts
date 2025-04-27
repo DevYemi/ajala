a25957bb3fb361678689807608674a272b94141e
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2025-04-27 12:05:45 +0100

    fix(react): set-uo root file

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"), // Entry point to your package
+      name: "react-ajala",
+      fileName: "react-ajala",
+      formats: ["es", "cjs"], // Output ES and CommonJS formats
+    },
+    rollupOptions: {
+      // Externalize React and ReactDOM
+      external: ["react", "react-dom"],
+    },
+  },
   plugins: [react()],
-})
+});
