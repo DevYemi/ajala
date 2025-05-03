0fb79213297a32d167e9892370b8bfe6811a23d4
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2025-05-03 12:40:55 +0100

    feat(react): re-export core types

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,14 @@ export {
   useAjalaJourneyContext,
 } from "./components/AjalaJourneyProvider";
 export type {
+  TAjalaEventTypes,
+  TAjalaOptions,
+  TAjalaSteps,
+  TSteps,
+  TTooltipPlacement,
+  TTransitionType,
+} from "ajala.js";
+export type {
   TReactAjalaCustomTooltipProps,
   TReactAjalaProviderProps,
 } from "./components/types";
